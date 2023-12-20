ee7522b64b963704a42d3ae6291b1c844f08c9a0
Ömer Kocabay <passanger54@hotmail.com>
2023-12-20 21:24:14 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Team.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Team
+    {
+        public int TeamID { get; set; }
+        public int PersonName { get; set; }
+        public int Ttitle { get; set; }
+        public int ImageUrl { get; set; }
+        public int FacebookUrl { get; set; }
+        public int InstagramUrl { get; set; }
+        public int WebSıteUrl { get; set; }
+        public int TwitterUrl { get; set; }
+    }
+}
